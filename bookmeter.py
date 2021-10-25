60cbdcf1c1e4d5afda76f2b1386639c0a6fb32d4
tetsuya111 <tetsuya1729@icloud.com>
2021-10-25 15:00:40 +0900

    first commit

---
+++ bookmeter.py
@@ -0,0 +1,119 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import time
+import urllib.parse as up
+import json
+
+HOST="bookmeter.com"
+URL="https://"+HOST
+
+SEARCH_USERS_URL=URL+"/users/search"
+
+
+HEADERS={
+	"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
+}
+
+class Selector:
+	BOOK="li.group__book"
+	THUMB=".book__thumbnail"
+	TITLE=".detail__title"
+	DATE=".detail__date"
+	AUTHOR=".detail__authors"
+	PAGE=".detail__page"
+
+
+def toData(book):
+	thumb=book.select_one(Selector.THUMB)
+	if thumb:
+		img=thumb.find("img")
+		imgUrl=img.get("src") if img else ""
+	else:
+		imgUrl=""
+	title_e=book.select_one(Selector.TITLE)
+	title=title_e.text if title_e else ""
+	href=title_e.find("a").get("href") if title_e else ""
+	date=book.select_one(Selector.DATE)
+	date=date.text if date else ""
+	author=book.select_one(Selector.AUTHOR)
+	author=author.text if author else ""
+	page=book.select_one(Selector.PAGE)
+	page=page.text if page else ""
+	return {
+		"imgUrl":imgUrl,
+		"title":title,
+		"href":href,
+		"date":date,
+		"author":author,
+		"page":page
+	}
+	
+
+def ___getBooks(soup):
+	return soup.select(Selector.BOOK)
+def __getBooks(soup):
+	return map(toData,___getBooks(soup))
+
+def _getBooks(url,page=1,session=requests):
+	params={
+		"page":page
+	}
+	res=session.get(url,headers=HEADERS,params=params)
+	soup=bs(res.text,"html.parser")
+	return __getBooks(soup)
+def getBooks(url,start=1,n=5):
+	session=requests.Session()
+	for page in range(start,start+n):
+		data=list(_getBooks(url,page,session))
+		if not data:
+			return
+		for dat in data:
+			dat["href"]=up.urljoin(url,dat["href"])
+			yield dat
+		time.sleep(1)
+
+def searchUsers(name):
+	headers={
+		**HEADERS
+		"refer":SEARCH_USERS_URL
+	}
+	params={
+		"name":name
+	}
+	res=requests.get(SEARCH_USERS_URL,headers=HEADERS,params=params)
+	#soup=bs(res.text,"html.parser")
+	data=json.loads(res.text)
+	return data["resources"]
+
+class User:
+	URL=URL+"/users/{0}"
+	class Type:
+		WANT="wish"
+		READ="read"
+		READING="reading"
+	def __init__(self,userid):
+		self.id=userid
+	@property
+	def url(self):
+		return self.URL.format(self.id)
+	def getBooks(self,type_,start=1,n=5):
+		url=self.url+"/books/"+type_
+		return getBooks(url,start,n)
+	@staticmethod
+	def make(name):
+		users=list(searchUsers(name))
+		if not users:
+			return None
+
+		for user in users:
+			if user["name"] == name:
+				return User(user["id"])
+		return None
+		
+
+
+
+if __name__ == "__main__": 
+	url="https://bookmeter.com/users/1035737/books/read"
+	for book in getBooks(url,1):
+		print(book)
